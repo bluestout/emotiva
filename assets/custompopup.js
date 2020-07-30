3776abc5ff4d974b73dd73a8b094512c4ac4cc45
FirstSun <foxysunstudio00aj@gmail.com>
2020-07-30 09:48:57 -0700

    before QA

--- assets/custompopup.js
+++ assets/custompopup.js
@@ -4,8 +4,12 @@ $(document).ready(function(){0
     $(".overlaydiv, .popupdiv").fadeOut();
   })
   $(".em-res-video-block-vid-play").click(function(){
+    var LinkGet = $(this).attr("data-link");
+    $(".VideoLink iframe").attr("src",LinkGet);
+    setTimeout(function(){ 
     $("body").addClass("em-pg-blur-sec");
     $(".overlaydiv, .popupdiv").fadeIn();
+      }, 1000);
     
   })
 })
