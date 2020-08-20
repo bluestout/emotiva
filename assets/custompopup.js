44a94ff234ded2adfc47cc43d5b5b80f2d3f2933
FirstSun <foxysunstudio00aj@gmail.com>
2020-08-20 10:57:56 -0700

    fix filtering and some QA cards

--- assets/custompopup.js
+++ assets/custompopup.js
@@ -2,6 +2,10 @@ $(document).ready(function(){0
   $(".overlaydiv, .closeimage").click(function(){
     $("body").removeClass("em-pg-blur-sec");
     $(".overlaydiv, .popupdiv").fadeOut();
+    $('.youtube-video').each(function(){
+        var el_src = $(this).attr("src");
+        $(this).attr("src",el_src);
+    });
   })
   $(".em-res-video-block-vid-play").click(function(){
     var LinkGet = $(this).attr("data-link");
